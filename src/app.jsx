785ea93d63313dc98b7676a3f8edccea26c63306
Kyle Robinson <ky.robinson72@gmail.com>
2025-04-07 00:09:37 -0600

    added import statements for the Play and Home elements to allow navigation using the navbar

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter, NavLink, Route, Routes } from "react-router";
+import { Home } from "./home/home";
+import { Play } from "./play/play";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./app.css";
 
