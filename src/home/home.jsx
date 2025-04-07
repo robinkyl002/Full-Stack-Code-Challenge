153aa13e0c0c28e8a92c03a5bb851763dd541703
Kyle Robinson <ky.robinson72@gmail.com>
2025-04-07 00:08:18 -0600

    Added Welcome message and button to navigate to the play element

---
+++ src/home/home.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router";
+
+export function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>
+        <h2>Welcome to WordGames!</h2>
+        <p>
+          Try to guess the word! For each turn, enter a single letter to see if
+          it is in the word. As you find correct letters, they will be displayed
+          in the word, while the rest of the letters are represented by blanks.
+          Be careful, though! Guess wrong 7 times and you lose!
+        </p>
+
+        <Button
+          variant="primary"
+          onClick={() => {
+            navigate("/play");
+          }}
+        >
+          Play Now!
+        </Button>
+      </div>
+    </main>
+  );
+}
